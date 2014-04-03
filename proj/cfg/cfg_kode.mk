399ad9f00c9e60812df836a6fc7cfb0b4bed8bef
U-fengjiayun-HP\fengjiayun <fengjiayun@fengjiayun-HP.(none)>
2014-04-03 17:33:11 +0800

    Feng Jiayun : get ready for other platform.

--- proj/cfg/cfg_kode.mk
+++ proj/cfg/cfg_kode.mk
@@ -13,28 +13,28 @@ KODE_MAP  := $(KODE_NAME).map
 KODE_TMP  := $(KODE_NAME).tmp
 KODE_ROOT := $(KODE_NAME)
 
+KODE_CPU_DIR := cpu
+KODE_CPU_LNX_DIR := x86_lnx
+KODE_CPU_SRC_S := cpu_head_s.gas cpu_gate_s.gas
+KODE_CPU_SRC_C := cpu_head_c.c   cpu_gate_c.c \
+                  cpu_hd.c \
+                  cpu_ps2.c \
+                  cpu_disp_text.c \
+                  cpu_disp_8bit.c \
+                  cpu_page.c \
+                  cpu_task.c \
+                  cpu_core.c \
+                  cpu_time.c 
+KODE_CPU_OBJ := $(patsubst %.gas, $(BUILD_ROOT)/$(KODE_ROOT)/$(KODE_CPU_DIR)/$(KODE_CPU_LNX_DIR)/%.o, $(KODE_CPU_SRC_S)) \
+                $(patsubst %.c,   $(BUILD_ROOT)/$(KODE_ROOT)/$(KODE_CPU_DIR)/$(KODE_CPU_LNX_DIR)/%.o, $(KODE_CPU_SRC_C))
+
+
 KODE_STARTUP_DIR := startup
-KODE_STARTUP_SRC_S := s_head.gas
 KODE_STARTUP_SRC_C := s_main.c
-KODE_STARTUP_OBJ := $(patsubst %.gas, $(BUILD_ROOT)/$(KODE_ROOT)/$(KODE_STARTUP_DIR)/%.o, $(KODE_STARTUP_SRC_S)) \
-                    $(patsubst %.c,   $(BUILD_ROOT)/$(KODE_ROOT)/$(KODE_STARTUP_DIR)/%.o, $(KODE_STARTUP_SRC_C))
+KODE_STARTUP_OBJ := $(patsubst %.c,   $(BUILD_ROOT)/$(KODE_ROOT)/$(KODE_STARTUP_DIR)/%.o, $(KODE_STARTUP_SRC_C))
+
 
 KODE_KERNEL_DIR := kernel
-
-KODE_KERNEL_CPU_DIR := cpu
-KODE_KERNEL_CPU_SRC_S := cpu_gate_s.gas
-KODE_KERNEL_CPU_SRC_C := cpu_gate_c.c \
-                         cpu_hd.c \
-                         cpu_ps2.c \
-                         cpu_disp_text.c \
-                         cpu_disp_8bit.c \
-                         cpu_page.c \
-                         cpu_task.c \
-                         cpu_core.c \
-                         cpu_time.c 
-KODE_KERNEL_CPU_OBJ := $(patsubst %.gas, $(BUILD_ROOT)/$(KODE_ROOT)/$(KODE_KERNEL_DIR)/$(KODE_KERNEL_CPU_DIR)/%.o, $(KODE_KERNEL_CPU_SRC_S)) \
-                       $(patsubst %.c,   $(BUILD_ROOT)/$(KODE_ROOT)/$(KODE_KERNEL_DIR)/$(KODE_KERNEL_CPU_DIR)/%.o, $(KODE_KERNEL_CPU_SRC_C))
-
 
 KODE_KERNEL_OS_DIR := os
 KODE_KERNEL_OS_SRC_S := 
@@ -131,8 +131,8 @@ KODE_SYSTEM_LIB_STD_SRC := vsprintf.c \
 KODE_SYSTEM_LIB_STD_OBJ := $(patsubst %.c, $(BUILD_ROOT)/$(KODE_ROOT)/$(KODE_SYSTEM_DIR)/$(KODE_SYSTEM_LIB_DIR)/$(KODE_SYSTEM_LIB_STD_DIR)/%.o, $(KODE_SYSTEM_LIB_STD_SRC))
 
 
-KODE_OBJ := $(KODE_STARTUP_OBJ) \
-            $(KODE_KERNEL_CPU_OBJ) \
+KODE_OBJ :=  \
+            $(KODE_CPU_OBJ) \
             $(KODE_KERNEL_OS_OBJ) \
             $(KODE_KERNEL_DRV_OBJ) \
             $(KODE_KERNEL_LIB_OBJ) \
@@ -141,14 +141,17 @@ KODE_OBJ := $(KODE_STARTUP_OBJ) \
             $(KODE_KERNEL_FONT_OBJ) \
             $(KODE_KERNEL_GUI_OBJ) \
             $(KODE_SYSTEM_LIB_KD_OBJ) \
-            $(KODE_SYSTEM_LIB_STD_OBJ)
-            
+            $(KODE_SYSTEM_LIB_STD_OBJ) \
+            $(KODE_STARTUP_OBJ)
 
 
 KODE_DBG := $(patsubst %.o, %.s, $(KODE_OBJ))
 KODE_DBG := $(subst build,debug, $(KODE_DBG))
 
+
 BUILD_DIR += $(BUILD_ROOT)/$(KODE_ROOT) \
+             $(BUILD_ROOT)/$(KODE_ROOT)/$(KODE_CPU_DIR) \
+             $(BUILD_ROOT)/$(KODE_ROOT)/$(KODE_CPU_DIR)/$(KODE_CPU_LNX_DIR) \
              $(BUILD_ROOT)/$(KODE_ROOT)/$(KODE_STARTUP_DIR) \
              $(BUILD_ROOT)/$(KODE_ROOT)/$(KODE_KERNEL_DIR) \
              $(BUILD_ROOT)/$(KODE_ROOT)/$(KODE_KERNEL_DIR)/$(KODE_KERNEL_CPU_DIR) \
@@ -163,9 +166,12 @@ BUILD_DIR += $(BUILD_ROOT)/$(KODE_ROOT) \
              $(BUILD_ROOT)/$(KODE_ROOT)/$(KODE_SYSTEM_DIR)/$(KODE_SYSTEM_LIB_DIR) \
              $(BUILD_ROOT)/$(KODE_ROOT)/$(KODE_SYSTEM_DIR)/$(KODE_SYSTEM_LIB_DIR)/$(KODE_SYSTEM_LIB_KD_DIR) \
              $(BUILD_ROOT)/$(KODE_ROOT)/$(KODE_SYSTEM_DIR)/$(KODE_SYSTEM_LIB_DIR)/$(KODE_SYSTEM_LIB_STD_DIR)
-             
+
+
 INC_DIR += -I$(SRC_ROOT)/$(KODE_ROOT) \
            -I$(SRC_ROOT)/$(KODE_ROOT)/include \
+           -I$(SRC_ROOT)/$(KODE_ROOT)/$(KODE_CPU_DIR) \
+           -I$(SRC_ROOT)/$(KODE_ROOT)/$(KODE_CPU_DIR)/$(KODE_CPU_LNX_DIR) \
            -I$(SRC_ROOT)/$(KODE_ROOT)/$(KODE_KERNEL_DIR)/$(KODE_KERNEL_CPU_DIR) \
            -I$(SRC_ROOT)/$(KODE_ROOT)/$(KODE_KERNEL_DIR)/$(KODE_KERNEL_OS_DIR) \
            -I$(SRC_ROOT)/$(KODE_ROOT)/$(KODE_KERNEL_DIR)/$(KODE_KERNEL_LIB_DIR) \
@@ -186,10 +192,8 @@ _IMAGE_KODE : $(MKIMG) $(MKFS) $(BOOT_BIN) $(KODE_BIN)
 	@echo "[Build][$(TARGET_FS)]"
 	@$(BUILD_ROOT)/$(MKFS) > $(TARGET_ROOT)/$(TARGET_FS)
 	@echo "[Build][$(TARGET_BIN)]"
-#	@$(BUILD_ROOT)/$(MKIMG) $(DEV_FLOPPY) $(BOOT_BIN) $(BUILD_ROOT)/$(KODE_BIN) > $(TARGET_ROOT)/$(TARGET_BIN)
 	@$(BUILD_ROOT)/$(MKIMG) $(DEV_FLOPPY) $(BOOT_BIN) $(BUILD_ROOT)/$(KODE_BIN) $(TARGET_ROOT)/$(TARGET_FS) > $(TARGET_ROOT)/$(TARGET_BIN)
 	@echo "[Build][$(TARGET_HD)]"
-#	@$(BUILD_ROOT)/$(MKIMG) $(DEV_HDISK) $(BOOT_BIN) $(BUILD_ROOT)/$(KODE_BIN) > $(TARGET_ROOT)/$(TARGET_HD)
 	@$(BUILD_ROOT)/$(MKIMG) $(DEV_HDISK) $(BOOT_BIN) $(BUILD_ROOT)/$(KODE_BIN) $(TARGET_ROOT)/$(TARGET_FS) > $(TARGET_ROOT)/$(TARGET_HD)
 
 $(KODE_BIN) : $(KODE_OBJ) $(KODE_DBG)
